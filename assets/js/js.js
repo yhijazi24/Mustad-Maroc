7f76cef77d6f058dfbe4efbe0765b0081c51cd3c
yhijazi24 <yahyahijazi643@gmail.com>
2023-12-12 20:05:18 +0100

    commit

--- assets/js/js.js
+++ assets/js/js.js
@@ -21,3 +21,47 @@ let swiperHome = new Swiper('.home__swiper', {
         }
     }
 })
+/*=============== TORE LOCATOR ===============*/
+const CONFIGURATION = {
+    "locations": [
+        { "title": "Mustad Maroc", "address1": "21 zone industrielle", "address2": "Settat, Maroc", "coords": { "lat": 32.980218828770774, "lng": -7.611912621179209 }, "placeId": "EhZSMzA42IwgU2V0dGF0LCBNb3JvY2NvIi4qLAoUChIJvyZYaxu2pQ0Rp5h5HkCo3G4SFAoSCZmeQqldBaYNERL63t6VXo0_" }
+    ],
+    "mapOptions": { "center": { "lat": 38.0, "lng": -100.0 }, "fullscreenControl": true, "mapTypeControl": false, "streetViewControl": false, "zoom": 4, "zoomControl": true, "maxZoom": 17, "mapId": "" },
+    "mapsApiKey": "AIzaSyAFOVo15-LD4_M9gERnCqTNZI_JAcmwV1c",
+    "capabilities": { "input": false, "autocomplete": false, "directions": false, "distanceMatrix": false, "details": false, "actions": false }
+};
+document.addEventListener('DOMContentLoaded', async () => {
+    await customElements.whenDefined('gmpx-store-locator');
+    const locator = document.querySelector('gmpx-store-locator');
+    locator.configureFromQuickBuilder(CONFIGURATION);
+});
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 100,
+})
+sr.reveal('.home__container', { delay: 100 })
+
+
---
+++ conts-assets/script.js
@@ -0,0 +1,48 @@
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+document.addEventListener('DOMContentLoaded', () => {
+    const productsLink = document.getElementById('productsLink');
+
+    productsLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'file:///C:/Users/Utente/Desktop/mustad%20maroc/index.html#product';
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const productsLink = document.getElementById('AboutLink');
+
+    productsLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'file:///C:/Users/Utente/Desktop/mustad%20maroc/index.html#about';
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const productsLink = document.getElementById('buyLink');
+
+    productsLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'file:///C:/Users/Utente/Desktop/mustad%20maroc/index.html#buy';
+    });
+});
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 100,
+})
+sr.reveal('.container-fluid', { delay: 100 })
+/*=============== CONTACT FORM VALIDATION ===============*/
+function submitForm() {
+    const firstName = document.getElementById('first_name').value;
+    const lastName = document.getElementById('last_name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const iam = document.getElementById('00N2400000EtJeo').value;
+    const contactAbout = document.getElementById('00N2400000Istrh').value;
+
+    if (!firstName || !lastName || !email || !phone || !iam || !contactAbout) {
+        const alertBox = document.querySelector('.alert')
+        return;
+    }
+}
+
